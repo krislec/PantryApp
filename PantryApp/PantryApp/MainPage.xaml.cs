d7b4792c70ec622e6e1aa4b4c1cf60bf6a344247
krislech <kristinslechticky@gmail.com>
2018-03-06 18:30:18 -0600

    Add project files.

---
+++ PantryApp/PantryApp/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PantryApp
+{
+	public partial class MainPage : ContentPage
+	{
+        int count = 0;
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public void ClickedButton(object sender, EventArgs args)
+        {
+            count++;
+            ((Button)sender).Text =
+                String.Format("{0} click{1}", count, count == 1 ? "" : "s");
+
+        }
+
+        public async void OnPantryDetailPage(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new PantryDetailPage());
+            }
+            catch (Exception EX)
+            {
+                ((Button)sender).Text = EX.Message;
+            }
+            
+        }
+
+        public async void ToPantryAddPage(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new PantryAdd());
+            }
+            catch (Exception EX)
+            {
+                ((Button)sender).Text = EX.Message;
+            }
+
+        }
+        public async void ToTestTabbedPage(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new TabbedMainPage());
+            }
+            catch (Exception EX)
+            {
+                ((Button)sender).Text = EX.Message;
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ PantryApp/PantryApp/PantryAdd.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PantryApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PantryAdd : ContentPage
+	{
+        int count2 = 100;
+		public PantryAdd ()
+		{
+			InitializeComponent ();
+		}
+        public void ClickedButton(object sender, EventArgs args)
+        {
+            count2++;
+            ((Button)sender).Text =
+                String.Format("{0} click{1}", count2, count2 == 1 ? "" : "s");
+
+        }
+        public async void ToManualAdd(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new ManualAdd());
+            }
+            catch (Exception EX)
+            {
+                ((Button)sender).Text = EX.Message;
+            }
+
+        }
+    }
+}
